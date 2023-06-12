36015a86251c5eefc5ae81569dd37777eef34528
calebcarnett <111102789+calebcarnett@users.noreply.github.com>
2023-06-12 14:25:30 -0400

    updated images to change on user click, need to add 4 new images to be used

--- taskflow/src/components/CreateTask.js
+++ taskflow/src/components/CreateTask.js
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/CreateTask.css";
-import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 
 export default function Create() {
   const [show, setShow] = useState(false);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [selectedImage, setSelectedImage] = useState(1);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const handleCreateBoard = async () => {
+    try {
+      const response = await fetch("/boards", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          title,
+          description,
+          backgroundImage: selectedImage,
+        }),
+      });
+
+      if (response.ok) {
+        // Board created successfully
+        handleClose();
+      } else {
+        // Handle error case
+        console.error("Failed to create board:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Failed to create board:", error);
+    }
+  };
+
+  const handleImageClick = (imageIndex) => {
+    setSelectedImage(imageIndex);
+  };
 
   return (
     <div className="create-board">
@@ -19,23 +49,52 @@ export default function Create() {
         <Modal.Header closeButton>
           <Modal.Title>Create Board</Modal.Title>
         </Modal.Header>
-        <img className="board-image"></img>
-        <label> Choose Background</label>
+        <img className={`board-image-first board-image-${selectedImage}`} />
+        <label>Choose Background</label>
         <div className="image-selection">
-          <img className="board-images"></img>
-          <img className="board-images"></img>
-          <img className="board-images"></img>
-          <img className="board-images"></img>
-          <img className="board-images"></img>
+          <img
+            className={`board-images-one ${
+              selectedImage === 1 ? "selected" : ""
+            }`}
+            onClick={() => handleImageClick("one")}
+          />
+          <img
+            src="image-2.jpg"
+            className={`board-images-two ${
+              selectedImage === 2 ? "selected" : ""
+            }`}
+            onClick={() => handleImageClick("two")}
+          />
+          <img
+            src="image-3.jpg"
+            className={`board-images-three ${
+              selectedImage === 3 ? "selected" : ""
+            }`}
+            onClick={() => handleImageClick("three")}
+          />
+          <img
+            src="image-4.jpg"
+            className={`board-images-four ${
+              selectedImage === 4 ? "selected" : ""
+            }`}
+            onClick={() => handleImageClick("four")}
+          />
+          <img
+            src="image-5.jpg"
+            className={`board-images-five ${
+              selectedImage === 5 ? "selected" : ""
+            }`}
+            onClick={() => handleImageClick("five")}
+          />
         </div>
         <label>Board Title</label>
-        <input></input>
-        <label className="required-title">Board Title is required</label>
+        <input value={title} onChange={(e) => setTitle(e.target.value)}></input>
+        <label>Board title is required.</label>
         <Modal.Footer>
           <Button
             variant="success"
             className="createboard-button"
-            onClick={handleClose}
+            onClick={handleCreateBoard}
           >
             Create
           </Button>
