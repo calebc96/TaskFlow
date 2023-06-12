4d71ac298c23d2d2d608c2b824cdcfebb095de51
calebcarnett <111102789+calebcarnett@users.noreply.github.com>
2023-06-12 18:52:41 -0400

    added all routes for viewing, updating and deleting boards

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,4 +1,4 @@
-const { Task } = require("../models");
+const { Task, Board } = require("../models");
 
 module.exports = {
   //retrieves all tasks
@@ -47,6 +47,58 @@ module.exports = {
         !tasks
           ? res.status(404).json({ message: "No thought with this id!" })
           : res.json(tasks)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  // Retrieve all boards
+  getBoards(req, res) {
+    Board.find()
+      .populate("tasks") // Populate tasks associated with each board
+      .then((allBoards) => res.json(allBoards))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // Retrieve a single board by ID
+  getSingleBoard(req, res) {
+    Board.findOne({ _id: req.params.boardId })
+      .populate("tasks") // Populate tasks associated with the board
+      .then((singleBoard) =>
+        !singleBoard
+          ? res.status(404).json({ message: "No board with that ID" })
+          : res.json(singleBoard)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // Create a new board
+  createBoard(req, res) {
+    Board.create(req.body)
+      .then((newBoardData) => res.json(newBoardData))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // Delete a board
+  deleteBoard(req, res) {
+    Board.findOneAndDelete({ _id: req.params.boardId })
+      .then((deletedBoard) =>
+        !deletedBoard
+          ? res.status(404).json({ message: "No board with this ID" })
+          : res.json(deletedBoard)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // Edit or update a board
+  updateBoard(req, res) {
+    Board.findOneAndUpdate(
+      { _id: req.params.boardId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((updatedBoard) =>
+        !updatedBoard
+          ? res.status(404).json({ message: "No board with this ID" })
+          : res.json(updatedBoard)
       )
       .catch((err) => res.status(500).json(err));
   },
