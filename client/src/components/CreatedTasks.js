d57837254e6c1d02866ea2b8a96851619064da7e
calebcarnett <111102789+calebcarnett@users.noreply.github.com>
2023-07-21 14:44:44 -0400

    added component to store task data and working on being able to edit tasks and add description, due dates and priority

--- client/src/components/CreatedTasks.js
+++ client/src/components/CreatedTasks.js
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from "react";
 import "../styles/CreatedTasks.css";
 import { CreateNewTask } from "../components/CreateNewTask";
+import ViewTasks from "../components/ViewTasks";
 
 export default function CreatedTasks({ boardId }) {
   const [tasks, setTasks] = useState([]);
   const [title, setTitle] = useState("");
+  const [taskid, setTaskId] = useState("");
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   useEffect(() => {
     loadTasks();
@@ -24,21 +30,36 @@ export default function CreatedTasks({ boardId }) {
 
   const handleTaskCreated = () => {
     loadTasks(); // Fetch the updated list of tasks when a new task is created
+    handleClose();
   };
 
   return (
-    <div className="created-tasks">
-      <div className="col-md-8">
-        <ul>
-          {tasks.map((task) => (
-            <li className="task-border" key={task._id}>
-              <p>{task.title}</p>
-              {/* <p>{task.description}</p> */}
-            </li>
-          ))}
-        </ul>
-        <CreateNewTask boardId={boardId} onTaskCreated={handleTaskCreated} />
+    <>
+      <div className="created-tasks">
+        <div className="col-md-8">
+          <ul>
+            {tasks.map((task) => (
+              <li
+                className="task-border"
+                key={task._id}
+                onClick={() => {
+                  handleShow();
+                  setTaskId(task._id);
+                }}
+              >
+                <p>{task.title}</p>
+              </li>
+            ))}
+          </ul>
+          <CreateNewTask
+            show={show}
+            boardId={boardId}
+            onTaskCreated={handleTaskCreated}
+            handleClose={handleClose} // Pass handleClose as a prop
+          />
+        </div>
+        {taskid && <ViewTasks taskid={taskid} />}
       </div>
-    </div>
+    </>
   );
 }
---
+++ client/src/components/ViewTasks.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+
+export default function ViewTasks({ taskid }) {
+  const [show, setShow] = useState(false);
+  const [tasks, setTasks] = useState([]);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  useEffect(() => {
+    getTasks();
+  }, [taskid]);
+
+  const getTasks = async () => {
+    try {
+      const response = await fetch(`/api/tasks/${taskid}`);
+      const data = await response.json();
+      console.log(data.task.title);
+      setTasks(data.task.title);
+      // Handle the retrieved data as needed
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <>
+      {tasks && (
+        <div
+          className="modal show"
+          style={{ display: "block", position: "initial" }}
+        >
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>
+                <p>{tasks}</p>
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </div>
+      )}
+    </>
+  );
+}
